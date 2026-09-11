78224dcdbfdf90fe39202bd6081dcb53b44a6c5a
Dominik Wendel <daw.wendel@gmail.com>
2026-09-11 12:03:19 +0200

    Implemented open and parse config file with nlohmann json

--- include/tkk-gw/adapters/s7adapter.hpp
+++ include/tkk-gw/adapters/s7adapter.hpp
@@ -3,6 +3,7 @@
 #include "types.hpp"
 #include "IDataSourceAdapater.hpp"
 #include "snap7micro/s7_micro_client.h"
+#include "nlohmann/json.hpp"
 
 /**
  * @brief Adapter for S7comm
@@ -16,12 +17,17 @@ private:
     const u32 slot;
     std::unique_ptr<TSnap7MicroClient> client;
     bool status;
+    nlohmann::json configData;
 
     void connect() const override;
     void disconnect() const override;
     bool getConnectedState() const override;
-    std::vector<DataPoint> readData() const;
-    void write() const;
+    std::vector<DataPoint> readData() const override;
+    void writeData() const override;
+
+    void openConfigFile(const std::string &path_);
+    void parseConfigFile(std::ifstream &file_);
+    void interpretConfigFile();
 
 public:
     S7Adapter(const char ip_, u32 rack_, u32 slot_);
--- src/adapters/s7adapter.cpp
+++ src/adapters/s7adapter.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include "tkk-gw/adapters/s7adapter.hpp"
 
 S7Adapter::S7Adapter(const char ip_, u32 rack_, u32 slot_) : ip{ip_},
@@ -12,6 +13,39 @@ S7Adapter::~S7Adapter()
 {
     this->client->Disconnect();
 }
+
+/**
+ * @brief *pens the adapter config file
+ *
+ * @param path_
+ */
+void S7Adapter::openConfigFile(const std::string &path_)
+{
+    std::ifstream file(path_); // std::ifstream is RAII -> closes itself when leaving scope
+    if (!file.is_open())
+    {
+        return;
+    }
+    this->parseConfigFile(file);
+}
+
+/**
+ * @brief Parses config file into nlohmann::json object
+ *
+ * @param file_
+ */
+void S7Adapter::parseConfigFile(std::ifstream &file_)
+{
+    this->configData = nlohmann::json::parse(file_);
+}
+
+/**
+ * @brief Interprets the config file
+ *
+ */
+void S7Adapter::interpretConfigFile() {
+
+};
 
 void S7Adapter::connect() const
 {
